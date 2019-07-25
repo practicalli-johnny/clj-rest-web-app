999bf5f1692c89fadf8d0c0da0481ffa383ffa69
Christian Paling <christian.paling@googlemail.com>
2019-07-25 13:08:24 +0200

    Add tests for user model

--- test/clj_rest_web_app/models/user_test.clj
+++ test/clj_rest_web_app/models/user_test.clj
@@ -7,7 +7,37 @@
 
 (use-fixtures :each fixtures/database-cleaner)
 
-(deftest row-count-test
-  (testing "Row count without query"
-    (sql/insert! db-url :users {:id 1 :email "test@mail.com" :password "12345"})
-    (is (= (row-count) 1))))
+(deftest row-count-wo-query-test
+  (sql/insert! db-url :users {:id 1 :email "test@mail.com" :password "12345"})
+  (is (= (row-count) 1)))
+
+(deftest row-count-with-query-test
+  (sql/insert! db-url :users {:id 1 :email "test@mail.com" :password "12345"})
+  (sql/insert! db-url :users {:id 2 :email "mail@test.com" :password "12345"})
+  (is (= (row-count "email LIKE ?" "test@%") 1)))
+
+(deftest find-many-wo-query-test
+  (sql/insert! db-url :users {:id 1 :email "test@mail.com" :password "12345"})
+  (sql/insert! db-url :users {:id 2 :email "mail@test.com" :password "12345"})
+  (let [users (find-many)]
+    (is (= (get users 0) {:id 1 :email "test@mail.com" :password "12345"}))
+    (is (= (get users 1) {:id 2 :email "mail@test.com" :password "12345"}))))
+
+(deftest find-many-with-query-test
+  (sql/insert! db-url :users {:id 1 :email "test@mail.com" :password "12345"})
+  (sql/insert! db-url :users {:id 2 :email "mail@test.com" :password "12345"})
+  (let [users (find-many "email LIKE ?" "test@%")]
+    (is (= (count users) 1))
+    (is (= (first users) {:id 1 :email "test@mail.com" :password "12345"}))))
+
+(deftest find-one-wo-query-test
+  (sql/insert! db-url :users {:id 1 :email "test@mail.com" :password "12345"})
+  (sql/insert! db-url :users {:id 2 :email "mail@test.com" :password "12345"})
+  (let [user (find-one)]
+    (is (= user {:id 1 :email "test@mail.com" :password "12345"}))))
+
+(deftest find-one-wo-query-test
+  (sql/insert! db-url :users {:id 1 :email "test@mail.com" :password "12345"})
+  (sql/insert! db-url :users {:id 2 :email "mail@test.com" :password "12345"})
+  (let [user (find-one "email LIKE ?" "%@test%")]
+    (is (= user {:id 2 :email "mail@test.com" :password "12345"}))))
